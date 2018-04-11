60a01bb5834f4d465b5246444142bfcc5bfbaa52
Prosper NGOUARI <prosperngouari38@gmail.com>
2018-04-11 01:14:25 -0700

    Calendrier en PHP

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Calendrier en PHP</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-dark bg-primary mb-3">
+	<a href="/index.php"class="navbar-brand">Mon Calendrier</a>
+</nav>
+
+<h1>Mars 2018</h1>
+</body>
+</html>
